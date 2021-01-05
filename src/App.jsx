6ea354d9347d666d91db1ec4d6be949484aa10a3
Francisco Masi <masifrancisco@gmail.com>
2021-01-05 17:35:57 -0300

    Initialize project using Create React App

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return <div>
+    <header>
+      <h1>CRA Template Francisco Masi</h1>
+    </header>
+  </div>
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+
+const StApp = styled(App)`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
