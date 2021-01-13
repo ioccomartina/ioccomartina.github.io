d015c18d205bae3ae839499a4ad22636bdb45df0
Francisco Masi <masifrancisco@gmail.com>
2021-01-13 18:32:03 -0300

    Maquetado footer completo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,13 +9,29 @@ export default function Footer() {
   return (
     <div className="footer">
       <div className="footer__wrapper">
-        <div className="footer__logo">
-          <Logo />
-        </div>
-        <div className="footer__info">
-          <b>Email:</b>
-          &nbsp;
-          {CONTACT.EMAIL}
+        <div className="footer__resume">
+          <div className="footer__resume-contact">
+            <div className="footer__logo">
+              <Logo />
+            </div>
+            <a
+              href={`mailto:${CONTACT.EMAIL}`}
+              className="footer__info"
+            >
+              <b>Email:</b>
+              &nbsp;
+              {CONTACT.EMAIL}
+            </a>
+          </div>
+          <div className="footer__resume-jobs">
+            <span className="footer__resume-title">Búsquedas</span>
+            <a href="" className="footer__resume-link">Front-end</a>
+            <a href="" className="footer__resume-link">Back-end</a>
+            <a href="" className="footer__resume-link">Full-stack</a>
+            <a href="" className="footer__resume-link">QA</a>
+            <a href="" className="footer__resume-link">Project Manager</a>
+            <a href="" className="footer__resume-link">Mobile</a>
+          </div>
         </div>
         <a
           className="footer__social-link"
